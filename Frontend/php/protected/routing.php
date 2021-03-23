c779cc50175e8c0297a38624e39138c3bd121159
Shuu <csaba19999@gmail.com>
2021-03-23 13:50:25 +0100

    Frontend routing megírva
    - Routing fájl létrehozva és megírva, feltöltve az eddigi adatokkal. Implementálva még nincs.
    - home.php és 404.php fájlok létrehozva a későbbi használathoz

---
+++ Frontend/php/protected/routing.php
@@ -0,0 +1,32 @@
+<?php 
+if(!array_key_exists('P', $_GET) || empty($_GET['P']))
+	$_GET['P'] = 'home';
+
+switch ($_GET['P']) {
+	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
+	case 'test': require_once PROTECTED_DIR.'normal/permission_test.php'; break;
+
+	case 'offers': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'normal/offers.php' : header('Location: index.php'); break;
+
+	case 'product': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'normal/product.php' : header('Location: index.php'); break;
+
+	case 'shoppingcart': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'normal/shoppingcart.php' : header('Location: index.php'); break;
+
+	case 'subscribe': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'normal/subscribe.php' : header('Location: index.php'); break;
+
+	case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
+
+	case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
+
+	case 'logout': IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
+
+	case 'addcreditcard': IsUserLoggedIn() ? require_once PROTECTED_DIR.'normal/addcreditcard.php' : header('Location: index.php'); break;
+
+	case 'forgetpassword': IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/forgetpassword.php' : header('Location: index.php'); break;
+
+	case 'changeprofiledatas': IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/changeprofiledatas.php' : header('Location: index.php'); break;
+
+	default: require_once PROTECTED_DIR.'normal/404.php'; break;
+}
+
+?>
