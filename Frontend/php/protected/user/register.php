f669b8bfdd8c9fc6c77939e9583964fe18a9f0ff
AJNCK7 <61760397+AJNCK7@users.noreply.github.com>
2021-04-11 12:32:06 +0200

    Regisztráció exceptionok (kis fejlesztésre szorul)
    A regisztráció során szükséges adatok helyességének ellenőrzése

--- Frontend/php/protected/user/register.php
+++ Frontend/php/protected/user/register.php
@@ -10,25 +10,30 @@
 				Felhasználónév <br></br>
 				<input type="text" class="registerinput" name="username" required> <br></br>
 				Jelszó <br></br>
-				<input type="password" class="registerinput" name="password" required> <br></br>
+				<input type="password" class="registerinput" name="password" minlength="8" required> <br></br>
 				E-mail <br></br>
 				<input type="email" class="registerinput" name="email" required> <br></br>
 				Teljes név <br></br>
-				<input type="text" class="registerinput" name="real_name" required> <br></br>
+				<input type="text" class="registerinput" name="real_name" minlength="5" required> <br></br>
 				Neme <br></br>
-                <input type="number" class="registerinput" name="sex" required> </br>
+                <select class="registerinput" name="sex" required>
+					<option value="">Neme</option>
+					<option value="0">Férfi</option>
+					<option value="1">Nő</option>
+					<option value="2">Egyéb</option>
+				</select> <br></br>
 				Születési dátum <br></br>
-				<input type="date" class="registerinput" name="birthdate" required> <br></br>
+				<input type='date' class="registerinput" name="date" min="1900-01-01" max="2021-01-01" required><br></br>
 				Irányítószám <br></br>
-				<input type="number" class="registerinput" name="zip_code" required> <br></br>
+				<input type="number" class="registerinput" name="zip_code" min="1000" max="9999" required> <br></br>
 				Város <br></br>
-				<input type="text" class="registerinput" name="city" required> <br></br>
+				<input type="text" class="registerinput" name="city" minlength="2" required> <br></br>
 				Út/utca <br></br>
-				<input type="text" class="registerinput" name="street" required> <br></br>
+				<input type="text" class="registerinput" name="street" minlength="2" required> <br></br>
 				Házszám <br></br>
-				<input type="number" class="registerinput" name="house_number" required> <br></br>
+				<input type="number" class="registerinput" name="house_number" min="1" required> <br></br>
 				Ország <br></br>
-				<input type="text" class="registerinput" name="country" required> <br></br>
+				<input type="text" class="registerinput" name="country" minlength="2" required> <br></br>
 				<button class="registersubmit" type="submit"> Regisztrálás </button> <br></br>
 			</form>
 				<a href="index.php?P=login" class="hrefunderline"> Rendelkezem felhasználóval </a> 
