3b56b95166530d1752ac267f1e59c8ef00f28b85
Maja Bódi <buksi.pajti@gmail.com>
2021-04-26 15:20:49 +0200

    Főoldal első reklám
    Elkészítve:
    - A model importálása
    - A model forgatása
    - Directional Light
    - Model pozíciója
    -Debug GUI az előzőekkel kiegészítve

--- Frontend/php/public/js/fooldal1.js
+++ Frontend/php/public/js/fooldal1.js
@@ -29,6 +29,77 @@ const textureLoader = new THREE.TextureLoader()
 const matcapTexture = textureLoader.load('./public/matcap/4.png')
 
 /**
+ * Lights
+ */
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0x438424, 0.206)
+directionalLight.position.set(2, 2, - 1)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+scene.add(directionalLight)
+
+directionalLight.castShadow = true
+console.log(directionalLight.shadow)
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 5
+directionalLight.shadow.radius = 10
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+directionalLightCameraHelper.visible = false
+scene.add(directionalLightCameraHelper)
+
+/**
+ * Models
+ */
+let model = null
+const gltfLoader = new GLTFLoader()
+gltfLoader.load(
+    './public/models/Duck/glTF/Duck.gltf',
+    (gltf) => {
+        console.log(gltf)
+        /*for(const child of gltf.scene.children){
+            scene.add(child)
+        }*/
+        /*while(gltf.scene.children.length){
+            scene.add(gltf.scene.children[0])
+        }*/
+
+        const child = gltf.scene
+        child.scale.set(1.2, 1.2, 1.2)
+        child.position.set(0.7, -1.3, 2.1)
+        child.rotation.y = 3.5
+        child.castShadow = true
+        scene.add(child)
+
+        gui.add(child.position, 'x').min(0).max(3).step(0.1).name('Model position x')
+        gui.add(child.position, 'y').min(-2).max(3).step(0.1).name('Model position y')
+        gui.add(child.position, 'z').min(0).max(3).step(0.1).name('Model position z')
+        
+        gui.add(child.rotation, 'y').name('Model rotation y').min(0).max(6).step(0.1)
+
+        gui.add(child.scale, 'x')
+        gui.add(child.scale, 'y')
+        gui.add(child.scale, 'z')
+
+        model = child
+    },
+    () => {
+        console.log('progress')
+    },
+    () => {
+        console.log('error')
+    }
+)
+
+/**
  * Fonts
  */
 const fontLoader = new THREE.FontLoader()
@@ -36,7 +107,7 @@ fontLoader.load(
     './public/fonts/helvetiker_regular.typeface.json',
     (font) => {
         const textGeometry = new THREE.TextGeometry(
-            'FullExtras furdokacsa',
+            'FullExtras Furdokacsa',
             {
                 font: font,
                 size: 0.75,
@@ -61,7 +132,7 @@ fontLoader.load(
     }
 )
 
-
+/*
 //Red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 'red'});
@@ -72,7 +143,7 @@ mesh.position.x = 0.8
 mesh.position.z = 2.2
 
 gui.add(mesh.position, 'x').name('Mesh position x').min(0).max(3).step(0.1)
-gui.add(mesh.position, 'z').name('Mesh position z').min(0).max(3).step(0.1)
+gui.add(mesh.position, 'z').name('Mesh position z').min(0).max(3).step(0.1)*/
 
 /**
  * Camera
@@ -88,7 +159,9 @@ gui.add(camera.position, 'x').name('Camera position x')
 gui.add(camera.position, 'z').name('Camera position z')
 gui.add(camera.rotation, 'y').name('Camera rotation y')
 
-//Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
@@ -96,11 +169,22 @@ renderer.setSize(sizes.width, sizes.height);
 
 renderer.render(scene, camera);
 
+/**
+ * Controls
+ */
+/*const controls = new OrbitControls(camera, canvas)
+controls.enabled = false
 
+gui.add(controls, 'enabled')*/
 
+/**
+ * Animate
+ */
 const tick = () =>
 {
-    mesh.rotation.y += 0.01;
+    //mesh.rotation.y += 0.01;
+    if(model !== null)
+        {model.rotation.y += 0.01;}
    // mesh.rotation.x += 0.01;
 
     //console.log('tick');
